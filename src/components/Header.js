10389aa67f5064671350121ab47d3f176fe039d8
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2022-06-26 14:13:42 +0700

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1>Call a Friend</h1>
+            <h4>your friendly contact app</h4>
+            <div className='underline'>
+                <span>.....................</span>
+            </div>
+        </div>
     )
 }
 
